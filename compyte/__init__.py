4878ce0be1edde792aad2356811b98abbf327a2b
Alfred <alfred@stensatter.se>
2024-10-08 19:46:41 +0200

    added 2 functions and made paths in components relative to them

--- compyte/__init__.py
+++
@@ -1,4 +0,0 @@
-# compyte/__init__.py
-from .compyte import parse
-
-__all__ = ['parse']
---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from compyter import render_content, render_file
+
+(render_file('./tests/index.html',{}))
